4076da565bed3987992b99ab2a01dfe5c797d48c
LiliyaLexx <liliya.fedyurina@gmail.com>
2024-05-06 15:49:55 -0700

    three weeks added

--- homework.mjs
+++ homework.mjs
@@ -4,7 +4,8 @@ const area = PI * radius * radius;
 const space = 0.8;
 let plants = 20;
 let plantsAfterWeek = 20 * 2;
-// let fullArea = space * plants * plantsAfterWeek;
+const fullArea = space * plantsAfterWeek;
+//one week
     if (plantsAfterWeek > 0.8 * area){
         console.log("Pruned")
  } else if( plantsAfterWeek >= 0.5 * area && plantsAfterWeek<=0.8 * area){
@@ -12,6 +13,33 @@ let plantsAfterWeek = 20 * 2;
  }else{
     console.log("Planted");
  }
+
+ //two weeks 
+ const plantsAfterTwoWeeks = plants * Math.pow(2,2);
+//  const fullAreaTwoWeeks = plantsAfterTwoWeeks * space;
+
+ if(plantsAfterTwoWeeks > 0.8 * area ){
+    console.log("Pruned");
+ } else if(plantsAfterTwoWeeks >= 0.5 * area && plantsAfterTwoWeeks <= 0.8 * area){
+    console.log("Monitored");
+ } else {
+    console.log("Planted");
+ }
+
+//three weeks
+
+const plantsAfterThreeWeeks = plants * Math.pow(2,3);
+
+if(plantsAfterThreeWeeks > 0.8 * area){
+    console.log("Pruned");
+     
+}else if(plantsAfterThreeWeeks >= 0.5 * area && plantsAfterThreeWeeks<= 0.8 * area ){
+    console.log("Monitored"); 
+
+} else{
+    console.log("Planted")
+}
+ 
 
  
 
